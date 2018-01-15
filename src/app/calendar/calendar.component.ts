7339beb872cff51ee80bca5dba4df441c3e42e1d
JKrymarys <kuba.krymarys@gmail.com>
2018-01-15 12:06:28 +0100

    events added

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,7 +10,39 @@ export class CalComponent implements OnInit {
   calendarOptions: Options;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
   constructor() {}
+
   ngOnInit() {
+    let e1 = {
+      title: 'The great opening marathon',
+      start: '2018-01-15',
+      end: '2018-01-18',
+    }
+    let e2 = {
+      title: 'Shape',
+      start: '2018-01-18',
+      end: '2018-01-18',
+    }
+    let e3 = {
+      title: 'Intro',
+      start: '2018-01-19',
+      end: '2018-01-19',
+    }
+    let e4 = {
+      title: 'Weightlifting',
+      start: '2018-01-24',
+      end: '2018-01-24',
+    }
+    let e5 = {
+      title: 'Shape',
+      start: '2018-01-23',
+      end: '2018-01-23',
+    }
+    let e6 = {
+      title: 'TRX',
+      start: '2018-01-26',
+      end: '2018-01-26',
+    }
+
      this.calendarOptions = {
         editable: true,
         eventLimit: false,
@@ -19,8 +51,12 @@ export class CalComponent implements OnInit {
           center: 'title',
           right: 'month,agendaWeek,agendaDay,listMonth'
         },
-       // events: data
+        events: [e1,e2,e3,e4,e5,e6]
       };
+
+     
+    //  this.ucCalendar.fullCalendar('renderEvent', el);
+      //this.ucCalendar.fullCalendar('rerenderEvents');
   }
 
 }
--- src/app/workouts/workouts-mock.ts
+++ src/app/workouts/workouts-mock.ts
@@ -2,6 +2,8 @@ import { Workout } from './workout';
 
 export const WORKOUTS: Workout[] = [
   { name: 'Shape', time: "90min", couch:"Adam Kowalski" },
-  { name: 'Crossfit', time: "75min", couch: "Andrzej Nowak" }
-
+  { name: 'Crossfit', time: "75min", couch: "Andrzej Nowak" },
+  { name: 'Intro', time: "60min", couch: "Krzysztof Podstawczyk" },
+  { name: 'Weightlifting', time: "80min", couch: "Anna Włodarczyk" },
+  { name: 'TRX', time: "45min", couch: "Andrzej Nowak" }
 ];
